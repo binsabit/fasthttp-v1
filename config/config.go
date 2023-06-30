cbd242cf0c911145ce167ae052a0f3082ce53198
binsabit <yerdaulet.seidizhappar@gmail.com>
2023-06-30 18:42:42 +0600

    .env file read

--- config/config.go
+++ config/config.go
@@ -1,7 +1,11 @@
 package config
 
 import (
+	"log"
 	"os"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
@@ -14,13 +18,20 @@ func Configure() Config {
 	return Config{
 		ServerPort: getEnv("SERVER_PORT", "8080"),
 		DB_DSN:     getEnv("DB_DSN", "postgres://yerdaulet:pa55word@localhost:5432/prosclad"),
-		LogFile:    getEnv("LOG_FILE", "../log/log.txt"),
+		LogFile:    getEnv("LOG_FILE", "./log/log.txt"),
 	}
 }
 
 func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
+	dir, err := os.Getwd()
+	if err != nil {
+		return fallback
 	}
-	return fallback
+	err = godotenv.Load(filepath.Join(dir, "config", "config.env"))
+	if err != nil {
+		log.Println(err)
+		return fallback
+
+	}
+	return os.Getenv(key)
 }
