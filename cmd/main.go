97d01d63773c407828331f8a4b675056547b52f3
binsabit <yerdaulet.seidizhappar@gmail.com>
2023-06-30 18:06:25 +0600

    gracefull shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -2,30 +2,17 @@ package main
 
 import (
 	"context"
+	"log"
+	"os/signal"
+	"syscall"
 
-	"github.com/binsabit/fasthttp-v1/config"
 	"github.com/binsabit/fasthttp-v1/internal/api"
-	"github.com/binsabit/fasthttp-v1/internal/data/postgesql"
-	"github.com/binsabit/fasthttp-v1/pkg"
 )
 
 func main() {
-
-	config := config.Configure()
-	logger := pkg.NewLogger(config.LogFile)
-
-	pool, err := postgesql.NewPGXPool(context.Background(), config.DB_DSN)
-	if err != nil {
-		logger.ErrorLog.Fatalf("could not establish db connetion pool: %v", err)
-	}
-	defer pool.Close()
-
-	pqStorage := postgesql.NewModels(pool)
-
-	app := api.NewAplication(config.ServerPort, pqStorage, logger)
-
-	err = app.Run()
-	if err != nil {
-		logger.ErrorLog.Fatalf("could not run application: %v", err)
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	defer cancel()
+	if err := api.Run(ctx); err != nil {
+		log.Fatal(err)
 	}
 }
--- internal/api/app.go
+++ internal/api/app.go
@@ -1,11 +1,19 @@
 package api
 
 import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/binsabit/fasthttp-v1/config"
+	"github.com/binsabit/fasthttp-v1/internal/data/postgesql"
 	types "github.com/binsabit/fasthttp-v1/internal/data/types"
 	"github.com/binsabit/fasthttp-v1/pkg"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
+
+var forceShutdownTime = time.Second * 15
 
 type Application struct {
 	serverPort string
@@ -23,11 +31,45 @@ func NewAplication(serverPort string, storage types.StorageInterface, logger pkg
 	}
 }
 
-func (app *Application) Run() error {
+func Run(ctx context.Context) error {
+	config := config.Configure()
+	logger := pkg.NewLogger(config.LogFile)
+
+	pool, err := postgesql.NewPGXPool(context.Background(), config.DB_DSN)
+	if err != nil {
+		return fmt.Errorf("could not establish db connetion pool: %v", err)
+	}
+	defer pool.Close()
+
+	pqStorage := postgesql.NewModels(pool)
+
+	app := NewAplication(config.ServerPort, pqStorage, logger)
 
 	app.router.Use(cors.New())
 	app.setupRoutes()
-	app.router.Listen(":" + app.serverPort)
 
-	return nil
+	go func() {
+		if err := app.router.Listen(":" + app.serverPort); err != nil {
+			app.logger.ErrorLog.Fatalf("could not server at port:%d, %v", app.serverPort, err)
+		}
+
+	}()
+	app.logger.InfoLog.Printf("listening to port$ %d", app.serverPort)
+
+	<-ctx.Done()
+
+	app.logger.InfoLog.Println("server gracefully shutting down")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), forceShutdownTime)
+	defer cancel()
+
+	if err := app.router.ShutdownWithContext(shutdownCtx); err != nil {
+		return fmt.Errorf("shutting down server: %v", err)
+	}
+
+	select {
+	case <-shutdownCtx.Done():
+		return fmt.Errorf("shutdown finished: %v", ctx.Err())
+	}
+
 }
