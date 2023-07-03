8795911bd97ae64f4db9c196ef6dc28e8f2bf51d
binsabit <yerdaulet.seidizhappar@gmail.com>
2023-07-03 16:48:58 +0600

    db connectio pool established with configuration

--- internal/data/postgesql/model.go
+++ internal/data/postgesql/model.go
@@ -3,14 +3,41 @@ package postgesql
 import (
 	"context"
 	"fmt"
+	"log"
 
+	"github.com/binsabit/fasthttp-v1/internal/config"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func NewPGXPool(ctx context.Context, connString string) (*pgxpool.Pool, error) {
-	pool, err := pgxpool.New(context.Background(), connString)
+func NewPGXPool(ctx context.Context, storageConf config.Storage) (*pgxpool.Pool, error) {
+	log.Println(storageConf)
+
+	dns := fmt.Sprintf(`user=%s password=%s host=%s port=%s 
+						dbname=%s sslmode=%s pool_max_conns=%d pool_min_conns=%d pool_max_conn_idle_time=%s`,
+		storageConf.User,
+		storageConf.Password,
+		storageConf.Host,
+		storageConf.Port,
+		storageConf.DBName,
+		storageConf.SSLMode,
+		storageConf.PoolMaxConns,
+		storageConf.PoolMinConns,
+		storageConf.PoolMaxConnIdleTime,
+	)
+	log.Println(dns)
+	poolConfig, err := pgxpool.ParseConfig(dns)
 	if err != nil {
-		return nil, fmt.Errorf("pgx connection error: %w", err)
+		return nil, err
 	}
-	return pool, nil
+
+	db, err := pgxpool.NewWithConfig(context.Background(), poolConfig)
+
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping(context.Background())
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- internal/data/postgesql/model_test.go
+++ internal/data/postgesql/model_test.go
@@ -2,19 +2,38 @@ package postgesql_test
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"testing"
 
 	"github.com/binsabit/fasthttp-v1/internal/config"
-	"github.com/binsabit/fasthttp-v1/internal/data/postgesql"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestConnectoin(t *testing.T) {
 	config := config.MustLoad()
+	storageConf := config.Storage
+	log.Println(storageConf)
 	// dsn := "postgres://yerdaulet:pa55word@localhost:5432/prosclad"
-	pool, err := postgesql.NewPGXPool(context.Background(), config.Storage)
+	dns := fmt.Sprintf(`user=%s password=%s host=%s port=%s 
+	dbname=%s sllmode=%s pool_max_conns=%d pool_min_conns=%d pool_max_conn_idle_time=%s`,
+		storageConf.User,
+		storageConf.Password,
+		storageConf.Host,
+		storageConf.Port,
+		storageConf.DBName,
+		storageConf.SSLMode,
+		storageConf.PoolMaxConns,
+		storageConf.PoolMinConns,
+		storageConf.PoolMaxConnIdleTime,
+	)
+	poolConfig, err := pgxpool.ParseConfig(dns)
 	assert.Nil(t, err)
 
-	err = pool.Ping(context.Background())
+	db, err := pgxpool.NewWithConfig(context.Background(), poolConfig)
+	assert.NotNil(t, db)
+	assert.Nil(t, err)
+	err = db.Ping(context.Background())
 	assert.Nil(t, err)
 }
